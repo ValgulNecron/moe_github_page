08f28fa3b4b7eefb814a7867a3ce3f77c8a9967e
valgul <valgulnecron@gmail.com>
2023-01-30 18:18:21 +0100

    better faster

--- js/anime.js
+++ js/anime.js
@@ -1,10 +1,17 @@
 async function getIMG() {
-let response = await fetch('https://web-production-0ec5.up.railway.app/https://anime-api.hisoka17.repl.co/img/waifu');
-    let json = await response.json();
-    let img = document.createElement("img");
-    img.src = json.url;
-    let container = document.getElementById("image-container");
-    container.appendChild(img);
+  const endpoints = [
+    'https://anime-api.hisoka17.repl.co/img/hug',
+    'https://anime-api.hisoka17.repl.co/img/pat',
+    'https://anime-api.hisoka17.repl.co/img/cuddle',
+    'https://anime-api.hisoka17.repl.co/img/waifu'
+  ];
+  let randomEndpoint = endpoints[Math.floor(Math.random() * endpoints.length)];
+  let response = await fetch(`https://web-production-0ec5.up.railway.app/${randomEndpoint}`);
+  let json = await response.json();
+  let img = document.createElement("img");
+  img.src = json.url;
+  let container = document.getElementById("image-container");
+  container.appendChild(img);
 }
 
 getIMG();
