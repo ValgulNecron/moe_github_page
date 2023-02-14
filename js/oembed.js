7192719faae01573999445988292e8f890c047fd
valgul <valgulnecron@gmail.com>
2023-02-14 15:47:33 +0100

    test

---
+++ js/oembed.js
@@ -0,0 +1,22 @@
+async function getIMG2() {
+    const endpoints = [
+        'https://anime-api.hisoka17.repl.co/img/hug',
+        'https://anime-api.hisoka17.repl.co/img/kiss',
+        'https://anime-api.hisoka17.repl.co/img/slap',
+        'https://anime-api.hisoka17.repl.co/img/wink',
+        'https://anime-api.hisoka17.repl.co/img/pat',
+        'https://anime-api.hisoka17.repl.co/img/kill',
+        'https://anime-api.hisoka17.repl.co/img/cuddle',
+    ];
+    let randomEndpoint = endpoints[Math.floor(Math.random() * endpoints.length)];
+    let response = await fetch(`https://web-production-0ec5.up.railway.app/${randomEndpoint}`);
+    let json = await response.json();
+    let img = document.createElement("img");
+    img.src = json.url;
+    let container = document.getElementById("image-container");
+    container.appendChild(img);
+    let meta = document.querySelector('meta[property="og:image"]');
+    meta.setAttribute("content", img.src);
+}
+
+getIMG2();
