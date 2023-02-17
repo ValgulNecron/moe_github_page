f470b57d39afe4f6167496dc8ea81635bde966ee
valgul <valgulnecron@gmail.com>
2023-02-18 00:21:40 +0100

    test

--- js/anime.js
+++ js/anime.js
@@ -1,15 +1,24 @@
 async function getIMG() {
     const endpoints = [
-        'https://anime-api.hisoka17.repl.co/img/hug',
-        'https://anime-api.hisoka17.repl.co/img/kiss',
-        'https://anime-api.hisoka17.repl.co/img/slap',
-        'https://anime-api.hisoka17.repl.co/img/wink',
-        'https://anime-api.hisoka17.repl.co/img/pat',
-        'https://anime-api.hisoka17.repl.co/img/kill',
-        'https://anime-api.hisoka17.repl.co/img/cuddle',
+        'waifu',
+        'neko',
+        'shinobu',
+        'megumin',
+        'bully',
+        'cuddle',
+        'cry',
+        'hug',
+        'kiss',
+        'pat',
+        'happy',
+        'dance',
+        'poke',
+        'slap',
+        'nom',
+
     ];
     let randomEndpoint = endpoints[Math.floor(Math.random() * endpoints.length)];
-    let response = await fetch(`${randomEndpoint}`, { mode: "no-cors" });
+    let response = await fetch(`https://api.waifu.pics/sfw/ ${randomEndpoint}`);
     let json = await response.json();
     let img = document.createElement("img");
     img.src = json.url;
